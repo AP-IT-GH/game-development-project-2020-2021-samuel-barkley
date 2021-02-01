2e34744a7dcc88752fe961067dee3a04b66ec924
Samuel <samuelbarkley2001@gmail.com>
2021-02-01 16:47:28 +0100

    Adding base program file
    Also add game instance to globalProps file

---
+++ GameDev2/Program.cs
@@ -0,0 +1,18 @@
+﻿using GameDev2.src;
+using System;
+
+namespace GameDev2
+{
+    public static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new Game1())
+            {
+                GlobalProps.GetGameInstance = game;
+                game.Run();
+            }
+        }
+    }
+}
